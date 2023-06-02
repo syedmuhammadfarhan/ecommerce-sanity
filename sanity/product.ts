63886dd6089502e16e5b61934f9d2ecb6d0862cf
syedmuhammadfarhan <farhanpiaic@gmail.com>
2023-06-02 12:00:36 +0500

    initial commit

---
+++ sanity/product.ts
@@ -0,0 +1,22 @@
+export const product = {
+  name: "product",
+  type: "document",
+  title: "Product",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Product Title",
+    },
+    {
+      name: "description",
+      type: "string",
+      title: "Product Description",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Product Image",
+    },
+  ],
+};
