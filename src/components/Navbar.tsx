869179e5d93beca87c50e099418d4c7bd23843f0
syedmuhammadfarhan <114781442+syedmuhammadfarhan@users.noreply.github.com>
2023-06-03 17:35:23 +0500

    empty page.tsx

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        //logo
+        //navbar items
+        //search box
+        //cart
+
+    </div>
+  )
+}
