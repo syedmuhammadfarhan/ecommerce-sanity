12349dbcc1a6ad82a2a19cb7b16e77fb0491cfc9
syedmuhammadfarhan <114781442+syedmuhammadfarhan@users.noreply.github.com>
2023-06-25 05:55:31 +0500

    after del API

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -3,8 +3,10 @@ import { db, cartTable } from "../../../drizzle/lib/drizzle";
 import { sql } from "@vercel/postgres";
 import {v4} from "uuid"
 import { cookies } from "next/headers";
+import { eq } from "drizzle-orm";
 
 export async function GET(request: NextRequest) {
+  // console.log(`from GET API route.ts`,cookies().get("user_id")?.value)
   try {
     // await sql`CREATE TABLE IF NOT EXISTS Todos(id serial primary key, Task varchar(255))`;
 
@@ -20,13 +22,12 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const req = await request.json();
-  console.log(req)
+  console.log(`this is POST req`,req)
   const uid = v4();
   if (!cookies().get("user_id")) {
-     cookies().set("user_id", uid)
-   }
-
-
+    cookies().set("user_id", uid)
+  }
+  
   try {
     const resPOST = await db
       .insert(cartTable)
@@ -44,4 +45,24 @@ export async function POST(request: NextRequest) {
       message: (error as { message: string }).message,
     });
   }
+}
+
+
+
+export async function DELETE(req: NextRequest) {
+    let url = req.nextUrl.searchParams;
+
+    try {
+        if (url.has("product_id")) {
+            let response = await db.delete(cartTable).
+                where(
+                    eq(cartTable.product_id, (url.get("product_id") as string))
+                    // and(eq(cartTable.product_id, (url.get("product_id") as string)), eq(cartTable.user_id, (url.get("user_id") as string)))
+                ).returning()
+            return NextResponse.json({ response });
+        }
+    } catch (error) {
+        console.log("error : ", (error as { message: string }).message)
+        return NextResponse.json({ error })
+    }
 }
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,71 +3,144 @@ import React, { useState } from "react";
 import { cartItems } from "@/drizzle/lib/drizzle";
 import { TiTrash } from "react-icons/Ti";
 import { useRouter } from "next/navigation";
-import { AiOutlineMinus, AiOutlinePlus } from "react-icons/Ai";
 import QuantityButton from "../components/QuantityButton";
 import { IProduct, getProductData } from "../components/cmsFetch";
 import { getData } from "./dbFetch";
+import { urlForImage } from "../../sanity/lib/image";
+import Image from "next/image";
 
 export default async function Cart() {
   const { refresh } = useRouter();
   const res: cartItems[] = await getData();
-  console.log(`this is cart db`, res[0].product_id);
+  console.log(`this is cart db after GET query`, res);
+  // const idToCompare = res.map((items) => items.product_id);
+  // console.log(`idToCompare`, idToCompare);
   const data: IProduct[] = await getProductData();
-  console.log(`this is cart cms`, data);
-  return (
-    <>
-      <div>
-        <h1 className="text-xl font-extrabold mt-12 mb-6">Shopping Cart</h1>
-      </div>
-      <div className="border border-blue-400 md:flex flex-between">
-        {/* product details */}
+  // console.log(`this is cart cms`, data);
 
-        <div className="border border-black flex flex-col w-full gap-y-8">
-          {res.map((items) => (
-            <div className="flex flex-col md:flex-row border border-green-500 p-1">
-              <div className="flex-none border border-red-400 h-24 w-20 md:h-48 md:w-44 rounded-lg">
-                image
-              </div>
-              <div className="md:ml-5 ml-0 border border-red-400 rounded-lg w-full flex flex-col justify-between">
-                <div className="flex justify-between">
-                  <div>title</div>
-                  <div className="hover:scale-110">
-                    <TiTrash size={25} />
+const handleDelete = async (product_id:string) => {
+  try {
+    if (product_id) {
+      const response = await fetch(`/api/cart?product_id=${product_id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        refresh();
+      } else {
+        console.log("Delete request failed with status:", response.status);
+      }
+    }
+  } catch (error) {
+    console.log("An error occurred during the delete request:", error);
+  }
+};
+
+
+  return (
+    <div>
+      {res.length ? (
+        <>
+          <div>
+            <h1 className="text-xl font-extrabold mt-12 mb-6">Shopping Cart</h1>
+          </div>
+          <div className="border border-blue-400 md:flex flex-between">
+            {/* product details */}
+
+            <div className="border border-black flex flex-col flex-between w-full">
+              {res.map((items) => (
+                <div className="flex flex-col md:flex-row border border-green-500 p-1">
+                  <div>
+                    {data
+                      .filter(
+                        (filteritem) => filteritem._id === items.product_id
+                      )
+                      .map((mapitem) => (
+                        <div className="flex overflow-hidden object-cover border border-red-400 h-24 w-20 md:h-48 md:w-44 rounded-lg">
+                          <Image
+                            src={urlForImage(mapitem.image[0]).url()}
+                            alt="productimage"
+                            width={300}
+                            height={300}
+                          />
+                        </div>
+                      ))}
+                  </div>
+                  <div className="md:ml-5 ml-0 border border-red-400 rounded-lg w-full flex flex-col justify-between">
+                    <div className="flex justify-between">
+                      <div>
+                        {data
+                          .filter(
+                            (filteritem) => filteritem._id === items.product_id
+                          )
+                          .map((mapitem) => (
+                            <div className="flex flex-col gap-y-2">
+                              <div className="text-lg font-extrabold">
+                                {mapitem.title}
+                              </div>
+                              <div className="text-slate-400 text-sm font-bold">
+                                {mapitem.generic.name}
+                              </div>
+                              <div className="text-lg font-bold">
+                                $ {mapitem.price}
+                              </div>
+                            </div>
+                          ))}
+                      </div>
+                      <div className="hover:scale-110" onClick={()=>handleDelete(items.product_id)}>
+                        <TiTrash size={25} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-extrabold text-sm">
+                        Delivery Estimation
+                      </div>
+                      <div className="text-pink-500 font-bold text-xs">
+                        5 Working Days
+                      </div>
+                    </div>
+                    {/* price and quantity div */}
+                    <div className="border flex justify-between">
+                      <div className="text-xs text-slate-500 flex items-center justify-center gap-x-6">
+                        <div>Qty: {items.quantity}</div>{" "}
+                        <div> Size: {items.size}</div>
+                      </div>
+                      <QuantityButton />
+                    </div>
+                    {/* price and quantity div ends */}
                   </div>
                 </div>
-                <div>generic.name</div>
-                <div className="font-bold">Delivery Estimation</div>
-                <div className="text-pink-500 font-bold">5 Working Days</div>
-                {/* price and quantity div */}
-                <div className="border flex justify-between">
-                  <div>Price</div>
-                  <QuantityButton />
+              ))}
+            </div>
+            {/* order summary div */}
+            <div className="bg-gray-300 rounded-lg mt-2 md:mt-0">
+              <div className="p-2 w-64 h-48 flex-none flex flex-col justify-between border-2">
+                <div className="border border-red-400">
+                  <h2 className="md:text-xl font-bold">Order Summary</h2>
                 </div>
-                {/* price and quantity div ends */}
+                <div className="border border-red-400 flex justify-between">
+                  <div>Items in Cart:</div>
+                  <div className="text-pink-500 font-bold">{res.length}</div>
+                </div>
+                <div className="border border-red-400 flex justify-between">
+                  <div>Sub Total:</div>
+                  <div>sum</div>
+                </div>
+                <div className="border bg-black text-white p-2 rounded-lg text-sm text-center">
+                  <button>Proceed to Checkout</button>
+                </div>
               </div>
             </div>
-          ))}
-        </div>
-        {/* order summary div */}
-        <div className="bg-gray-300 rounded-lg mt-2 md:mt-0">
-          <div className="p-2 w-64 h-48 flex-none flex flex-col justify-between border-2">
-            <div className="border border-red-400">
-              <h2 className="md:text-xl font-bold">Order Summary</h2>
-            </div>
-            <div className="border border-red-400 flex justify-between">
-              <div>Quantity:</div>
-              <div>sum</div>
-            </div>
-            <div className="border border-red-400 flex justify-between">
-              <div>Sub Total:</div>
-              <div>sum</div>
-            </div>
-            <div className="border bg-black text-white p-2 rounded-lg text-sm text-center">
-              <button>Proceed to Checkout</button>
-            </div>
           </div>
+        </>
+      ) : (
+        <div className="text-3xl text-slate-400 font-bold text-center pt-10">
+          No Items Available
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 }
